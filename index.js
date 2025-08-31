64f461189807f7002a66a93e6f7e5502ce2bd867
anacarmona10 <anacarmona1023@gmail.com>
2025-08-31 18:16:29 -0500

    Implementación de la función para restar

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function sumar(num1, num2) {
   return num1 + num2;
 }
 
+function restar(num1, num2) {
+  return num1 - num2;
+}
+
 // Función principal del programa
 function calculadora() {
   var opcion = prompt(
